37b0a9e0e61a133edf0a1a5b14b576e970279864
hairulysin <90087096+hairulysin@users.noreply.github.com>
2023-03-26 14:31:01 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,63 +1,41 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+// Hairul Yasin
+// F55121011
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
 
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'Welcome To the API'
-    });
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('viewn engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.post('/api/posts', verifyToken, (req, res) => {
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
-        if(err){
-            res.sendStatus(403);
-        } else{
-            res.json({
-                message: 'Post Created...', 
-                authData
-            });
-        }
-    });
-    
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.post('/api/login', (req, res) => {
-    //mock user
-    const user = {
-        id:1,
-        username: 'hairul',
-        email: 'binabillakk@gmail.com'
-    }
-    jwt.sign({user}, 'secretkey', { expiresIn: '60s' }, (err, token) => {
-        res.json({b
-    
-        });
-    });
-});
-
-// Format of token
-// Authorization bearer <access_Token>
-
-//Verify TOken
-function verifyToken(req, res, next){
-    //Get auth header value
-    const bearerHeader = req.headers['authorization'];
-    // Check if bearer is undefined
-    if(typeof bearerHeader !== 'undefined') {
-        //Split at the space
-        const bearer = bearerHeader.split(' ');
-        // Get TOken from array
-        const bearerToken = bearer[1];
-        // Set the token
-        req.token = bearerToken;
-        // Next middleware
-        next();
-    } else {
-        //Forbidden
-        res.sendStatus(403);
-    }
-}
-
-app.listen(5000 , () => console.log('Server started on port 5000'));
+module.exports = app;
